56410cb7088c246c88492387b47fa51ada076a09
Ben Jolitz <ben.jolitz@xad.com>
2015-04-15 18:16:48 -0700

    Update travis config

--- setup.py
+++ setup.py
@@ -10,10 +10,39 @@ except ImportError:
 
 with open("README.rst", 'r') as fh:
     description = fh.read()
+import sys
+
+if not sys.version_info < (3, 0, 0):
+    raise ImportError("Does not run on Python 3")
 
 setup(
     name='trollius_redis',
     author='Jonathan Slenders, Ben Jolitz',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Databases',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # Python 3 doesn't work until we can make pubsub bytes work.
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+    ],
     version='0.0.1',
     license='LICENSE.txt',
     url='https://github.com/benjolitz/trollius-redis',
